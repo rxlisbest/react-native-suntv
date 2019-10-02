2dad2689391ed7181d2234baaf90f6791481c4ff
Roy <rxlisbest@gmail.com>
2019-10-02 13:29:08 +0800

    react-native-element

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  Button,
+  ThemeProvider
+} from 'react-native-elements'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <ThemeProvider>
+        <Button title="Hey!" />
+      </ThemeProvider>
     </View>
   );
 }
