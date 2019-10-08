c078c6197c11a967c88b9f5791c6080cf5566ee7
Roy <rxlisbest@gmail.com>
2019-10-08 12:50:15 +0800

    expo av

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component, PropTypes }  from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  requireNativeComponent
 } from 'react-native';
 import { connect } from "react-redux";
 import {
   Button,
   ThemeProvider
 } from 'react-native-elements'
-import Video from 'react-native-video'
+import { Audio, Video } from 'expo-av';
 import { Header } from 'react-native-elements';
 
 export default function App() {
@@ -20,6 +21,16 @@ export default function App() {
         centerComponent={{ text: '首页', style: { color: '#fff' } }}
         rightComponent={{ icon: 'home', color: '#fff' }}/>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Video
+        source={{ uri: 'http://vjs.zencdn.net/v/oceans.mp4' }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+        style={{ width: 300, height: 300 }}
+      />
       <ThemeProvider>
         <Button title="Hey!" />
       </ThemeProvider>
